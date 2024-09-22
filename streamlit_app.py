73d5a0e0113db8c14f19f3495bc33e671cbc2290
marcus4work <141764554+marcus4work@users.noreply.github.com>
2024-09-21 23:54:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+https://cropdata-mvp-test1.streamlit.app/
